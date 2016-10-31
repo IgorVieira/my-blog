dacd58d01cf68e1c3876fac69d18eb20a7448e72
Igor Vieira <igor.p.r.vieira@gmail.com>
2016-10-31 17:20:28 -0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+let toogleMenu = document.querySelectorAll('.toggle-menu'),
+    wrapper    = document.querySelector('.wrapper');
+
+for (let i = 0; i < toogleMenu.length; i++){
+    toogleMenu[i].addEventListener('click', menuAction);
+}
+
+document.addEventListener('keyup', (e =>{
+    if(e.keyCode == 27) {
+        if(wrapper.classList.contains('show-menu')){
+            menuAction();
+        }
+    }
+}));
+
+function menuAction() {
+    if(wrapper.classList.contains('show-menu')){
+        wrapper.classList.remove('show-menu');
+    }
+    else {
+        wrapper.classList.add('show-menu');
+    }
+}
